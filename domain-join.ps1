da8082cc1f323e44a5ffc1a4d38323c0cbe54f52
Nathaniel Churchill <natechurchill16@gmail.com>
2026-07-17 17:59:48 +0000

    ddd

--- domain-join.ps1
+++ domain-join.ps1
@@ -1,10 +1,17 @@
 # Run as Administrator
 
+# Enable firewall groups required by Invoke-GPUpdate
 Enable-NetFirewallRule -DisplayGroup "Remote Scheduled Tasks Management"
 Enable-NetFirewallRule -DisplayGroup "Windows Management Instrumentation (WMI)"
 Enable-NetFirewallRule -DisplayGroup "Remote Service Management"
 Enable-NetFirewallRule -DisplayGroup "File and Printer Sharing"
 
+# Ensure Task Scheduler is running
+Start-Service Schedule
+
+# Ensure RPC-related services are running
+Start-Service RpcSs
+Start-Service Winmgmt
 
 $NewComputerName = 'REPLACE_ME'
 $DomainName      = 'compy.local'
