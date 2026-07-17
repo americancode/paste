595e3e13998988b5f9b680f7cf79ce94e4c4bb1a
Nathaniel Churchill <natechurchill16@gmail.com>
2026-07-17 00:36:06 +0000

    tt

---
+++ force-gpo.ps1
@@ -0,0 +1,3 @@
+Invoke-GPUpdate -Computer "dc01-mssql-101" -Target Computer -Force -RandomDelayInMinutes 0
+
+Invoke-GPUpdate -Computer "dc01-mssql-102" -Target Computer -Force -RandomDelayInMinutes 0
