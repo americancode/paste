ed47c940c12a0b5323605222317a7ad339e8dac5
Nathaniel Churchill <natechurchill16@gmail.com>
2026-07-17 00:24:05 +0000

    tt

--- sample.ps1
+++ sample.ps1
@@ -1,4 +1,4 @@
 .\Configure-SqlAnsible.ps1 `
     -TargetOuDn 'OU=SQL,OU=Servers,DC=compy,DC=local' `
-    -ServiceAccountsOuDn 'OU=ServiceAccounts,DC=compy,DC=local' `
+    -ServiceAccountsOuDn 'OU=Service Accounts,DC=compy,DC=local' `
     -WinRmIPv4Filter '*'
