1d57dcd50830479aee61c6b9e7d3d79ac847646e
Nathaniel Churchill <natechurchill16@gmail.com>
2026-07-17 18:46:53 +0000

    asdf

--- unjoin.ps1
+++ unjoin.ps1
@@ -6,3 +6,26 @@ Remove-Computer `
     -Verbose `
     -Restart `
     -Force
+
+
+   # ------------
+$computerSystem = Get-CimInstance Win32_ComputerSystem
+
+Invoke-CimMethod `
+    -InputObject $computerSystem `
+    -MethodName UnjoinDomainOrWorkgroup `
+    -Arguments @{
+        Password        = $null
+        UserName        = $null
+        FUnjoinOptions  = 0
+    }
+
+Invoke-CimMethod `
+    -InputObject $computerSystem `
+    -MethodName JoinDomainOrWorkgroup `
+    -Arguments @{
+        Name          = "WORKGROUP"
+        FJoinOptions  = 0
+    }
+
+Restart-Computer -Force
