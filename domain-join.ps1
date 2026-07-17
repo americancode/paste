836bbbdef9d1a6149e7688a3f6d117c7539f465c
Nathaniel Churchill <natechurchill16@gmail.com>
2026-07-17 00:28:04 +0000

    domain-join

--- domain-join.ps1
+++ domain-join.ps1
@@ -1,8 +1,21 @@
-$NewComputerName = 'REPLACE_ME'
+# Run as Administrator
+
+$NewComputerName = 'SQLVM01'
 $DomainName      = 'compy.local'
 $OUPath          = 'OU=SQL,OU=Servers,DC=compy,DC=local'
+$DnsServer       = '172.21.4.100'
 
-$Credential = Get-Credential -Message "Enter credentials allowed to join $DomainName"
+# Set DNS server on all active adapters
+Get-NetAdapter | Where-Object Status -eq 'Up' | ForEach-Object {
+    Set-DnsClientServerAddress `
+        -InterfaceIndex $_.InterfaceIndex `
+        -ServerAddresses $DnsServer
+}
+
+# Verify domain can be resolved
+Resolve-DnsName $DomainName -Server $DnsServer
+
+$Credential = Get-Credential -Message "Enter credentials to join $DomainName"
 
 Add-Computer `
     -DomainName $DomainName `
